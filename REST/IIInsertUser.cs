769fae592857c4491aba4f0855f53431e999b84e
RemsWeb <109938038+RemsWeb@users.noreply.github.com>
2022-07-26 22:02:03 +0700

    Add files via upload

---
+++ REST/IIInsertUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MITT_HIMAWAN_SUTANTO.REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IIInsertUser" in both code and config file together.
+    [ServiceContract]
+    public interface IIInsertUser
+    {
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+           UriTemplate = "/InsertUser", Method = "POST")]
+        List<InsertMessage> InsertUser(string Usernames, string Names, string Passwords, string Addresss, string DOBs, string Emails);
+    }
+
+    [DataContract]
+    public class GetDetail
+    {
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string DOB { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+    }
+
+    [DataContract]
+    public class InsertMessage
+    {
+        [DataMember]
+        public string MSG { get; set; }
+
+    }
+}
---
+++ REST/IInsertUser.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MITT_HIMAWAN_SUTANTO.DataAccess;
+
+namespace MITT_HIMAWAN_SUTANTO.REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "IInsertUser" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select IInsertUser.svc or IInsertUser.svc.cs at the Solution Explorer and start debugging.
+    public class IInsertUser : IIInsertUser
+    {
+        InsertBase oInsert = new InsertBase();
+
+        public List<InsertMessage> InsertUser(string Usernames, string Names, string Passwords, string Addresss, string DOBs, string Emails)
+        {
+
+            List<InsertMessage> GetDetail_Data = new List<InsertMessage>();
+            InsertMessage data = new InsertMessage();
+
+            DataTable dtInsert = new DataTable();
+            dtInsert = oInsert.f_sp_INSERT_USER_PROFILE(Usernames, Names, Passwords, Addresss, DOBs, Emails);
+
+            if (dtInsert.Rows.Count > 0)
+            {
+                GetDetail_Data.Clear();
+
+                foreach (DataRow item in dtInsert.Rows)
+                {
+                    data.MSG = item["MSG"].ToString();
+                    GetDetail_Data.Add(data);
+                }
+            }
+            else
+            {
+                data.MSG = "";
+
+            }
+
+
+            return GetDetail_Data;
+        }
+    }
+}
